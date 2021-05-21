37cd45b2306680bb3c5faf140a7d1cd30f487d03
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-05-21 16:34:10 -0400

    update

--- src/main.js
+++ src/main.js
@@ -18,20 +18,19 @@ let noiseInc = .025;
 
 let init = () => {
   noise.seed(Math.round(Math.random() * 65536));
-  console.log(noise);
   renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 
   aspect = window.innerWidth / window.innerHeight;
   near = 0.1;
-  far = 100;
+  far = 200;
   fov = 75;
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.z = 40;
+  camera.position.z = 60;
   camera.position.y = 20;
   camera.position.x = 0;
 
   controls = new THREE.OrbitControls( camera, renderer.domElement );
-  controls.target.set(16,5,16)
+  controls.target.set(24,5,24)
   controls.update();
   
 
@@ -42,7 +41,7 @@ let init = () => {
     const color = 0xffffff;
     const intensity = 1;
     const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-8, 32, -8);
+    light.position.set(-8, 16, -8);
     const light2 = new THREE.DirectionalLight(color, intensity);
     light2.position.set(40, 16, 40);
     scene.add(light);
@@ -54,14 +53,14 @@ let init = () => {
   let xOff = 0.01;
   let yOff = 0.01;
   let zOff = 0.01;
-  for (let xN = 0; xN < 32; xN++) {
+  for (let xN = 0; xN < 48; xN++) {
     console.log(noise.perlin3(xOff, yOff, zOff));
     world.push([]);
-    for(let zN = 0; zN < 32; zN++) {
+    for(let zN = 0; zN < 48; zN++) {
       world[xN].push([]);
-      for(let yN = 0; yN < Math.abs(noise.perlin3(xOff, yOff, zOff) * 32) + 5; yN++) {
+      for(let yN = 0; yN < Math.abs(noise.perlin3(xOff, yOff, zOff) * 32) + 1; yN++) {
         let block;
-        if(yN <= 5) {
+        if(yN <  Math.floor(Math.abs(noise.perlin3(xOff, yOff, zOff) * 32)/2) + 1) {
           block = new Block(xN, yN, zN, scene, 1);
         } else {
           block = new Block(xN, yN, zN, scene, 0);
